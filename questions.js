8ad6fc30e48431eeead4356fee90b02027954a3e
Jeremy White <65377062+Psyriel@users.noreply.github.com>
2020-06-14 22:25:58 -0500

    uploading project

---
+++ questions.js
@@ -0,0 +1,34 @@
+// function loadQuestions() {
+const questions = [{
+        title: "Question 1: The logical operator that represents 'or' is ____.",
+        choices: ["1. ||", "2. OR", "3. &&", "4. ==="],
+        answer: "1. ||"
+    },
+
+    {
+        title: "Question 2: The condition in an if / else statement is enclosed within ____.",
+        choices: ["1. quotes", "2. curly brackets", "3. parentheses", "4. square brackets"],
+        answer: "3. parentheses"
+    },
+
+    {
+        title: "Question 3: A property of an object that is a function is called a ____.",
+        choices: ["1. method", "2. string", "3. stylesheet", "4. boolean"],
+        answer: "1. method"
+    },
+
+    {
+        title: "Question 4: The instructions for a function are enclosed within ____.",
+        choices: ["1. quotes", "2. curly brackets", "3. parentheses", "4. square brackets"],
+        answer: "2. curly brackets"
+    },
+
+    {
+        title: "Question 5: Commonly used data types DO NOT include:",
+        choices: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
+        answer: "3. alerts"
+
+    }
+];
+// };
+// loadQuestions();
